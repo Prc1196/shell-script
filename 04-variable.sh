71ac85ebd979a6d3964e8a23e55912762602dd55
Pradeepreddyc <prc1196@gmail.com>
2025-01-12 22:10:08 +0530

    conversation added

--- 04-variable.sh
+++ 04-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Ram # no sapce before and after equal
-PERSON2=Suresh
+PERSON1=$1 # no sapce before and after equal
+PERSON2=$2
 
 echo "${PERSON1}:: Hi $PERSON2"
 echo "${PERSON2}:: Hello $PERSON1"
