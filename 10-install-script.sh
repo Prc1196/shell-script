cd465c0dc57b4954f8e7e2a6ba643fe2e9b13ae8
Pradeepreddyc <prc1196@gmail.com>
2025-01-13 21:40:38 +0530

    shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,5 +8,23 @@ then
     exit 1 #other than 0
 fi
 
-dnf install mysql -y
+dnf install mysqll -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else 
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing Git ... FAILURE"
+    exit 1
+else 
+    echo "Installing Git ... SUCCESS"
+fi
 
