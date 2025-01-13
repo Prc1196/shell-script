64b8da050ae6da2e1c57a89db982f03af0886775
Pradeepreddyc <prc1196@gmail.com>
2025-01-13 20:49:26 +0530

    conversation added

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER=$1
+# -gt, -lt, -ge, -le, -eq
+if [ $NUMBER -gt 100 ]
+then
+    echo "given number is greater than 100"
+else
+    echo "given number is lessthan or equal to 100"    
+fi     
