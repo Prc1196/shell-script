8941320beed31a06a5c747a101244989fe5e7b09
Pradeepreddyc <prc1196@gmail.com>
2025-01-13 21:20:34 +0530

    shell-script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -5,6 +5,4 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have the sudo access to execute this srcipt"
-else
-    echo "SHOW:: root user access command to access to exicute this script"
 fi
