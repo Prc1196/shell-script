d437ba55ad5d42773b0407b81edd8614c5c295df
Pradeepreddyc <prc1196@gmail.com>
2025-01-16 21:25:28 +0530

    expense shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 #real projects will be moniter for 70
 
 while read -r line
 do
-    echo $line
+    USAGE=$(echo $line | awk -F " " '{print $6}')
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "partition: $PARTITION , usage: $USAGE"
 done <<< $DISK_USAGE
