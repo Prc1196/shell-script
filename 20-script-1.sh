f653fcf2833d4c05f268014e522fecf7eb43a7f1
Pradeepreddyc <prc1196@gmail.com>
2025-01-20 16:54:51 +0530

    expense shell

--- 20-script-1.sh
+++ 20-script-1.sh
@@ -3,8 +3,9 @@
 MSG="script-1"
 GREET="Hello from script-1"
 source ./21-script-2.sh
-echo "Hello from: $MSG"
 
+echo "Hello from: $MSG"
+echo "A value: $A"
 
 #sh 21-script-2.sh
 
--- 21-script-2.sh
+++ 21-script-2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MSG="script-2"
-
+MSG="SCRIPT-2"
+A="10"
 echo "Hello from: $MSG"
 
 echo "Greeting: $GREET"
